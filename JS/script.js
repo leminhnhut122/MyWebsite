a318e1433f10f11eb1f3fbb9f254d43cf2f3f357
le minh nhut <151102634+leminhnhut122@users.noreply.github.com>
2024-06-17 07:37:32 +0700

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var menuIcon = document.getElementById('menu-icon');
-  var menu = document.getElementById('menu');
-
-  menuIcon.addEventListener('click', function() {
-      menu.classList.toggle('show');
+    var menuIcon = document.getElementById('menu-icon');
+    var menu = document.getElementById('menu');
+  
+    menuIcon.addEventListener('click', function() {
+        menu.classList.toggle('show');
+    });
   });
-});
